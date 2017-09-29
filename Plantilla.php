c950e620ee9ae034523e3912924d94f18c13c502
DORIAN_A\Darius Aguilera <jdario_cba@hotmail.com>
2017-09-29 01:13:45 -0300

    se agrega un archivo ccs se elimina link no utilizados

--- Plantilla.php
+++ Plantilla.php
@@ -89,16 +89,13 @@
   
 	<!-- container javascripts start-->
   	<section id="container">
-	    <script src="js/jquery.js"></script>
-		<script src="js/jquery-1.8.3.min.js"></script>
-	    <script type="text/javascript" src="js/jquery-ui-1.9.2.custom.min.js"></script>
-	    <!-- bootstrap -->
-	    <script src="js/bootstrap.min.js"></script>
+	    <script src="js/jquery-1.8.3.min.js"></script>
 	    <!-- nice scroll -->
-	    <script src="js/jquery.scrollTo.min.js"></script>
 	    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
 	    <!--custome script for all page-->
     	<script src="js/scripts.js"></script>	   
+	    <!-- bootstrap -->
+	    <script src="js/bootstrap.min.js"></script>
 	</section>
 	         
 	<!-- container javascripts end-->
