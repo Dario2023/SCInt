96a38112b39bf6c87e97a4353144eedbfe0df800
DORIAN_A\Darius Aguilera <jdario_cba@hotmail.com>
2017-10-11 23:03:03 -0300

    actualizacion de formulario gestor persona

--- gestor_persona.php
+++ gestor_persona.php
@@ -6,17 +6,15 @@
 	
 	<section id="main-content">
         <section class="wrapper">            
-	    <div class="row">
-	    	<div class="col-lg-12">
-				<h3 class="page-header"><i class="fa fa-file-text-o"></i>PERSONAS</h3>
-				
-			</div>
-            <div class="panel-body">
-                <form class="form-horizontal " method="get">
+	    	<div class="row">
+	    		<div class="col-lg-12">
+					<h3 class="page-header"><i class="fa fa-file-text-o"></i>PERSONA</h3>
+				</div>
+            	<div class="panel-body">
                     <div class="form-group">
-                        
-                            <div class="row">
-                                <div class="col-lg-6">
+                        <div class="col-lg-12">
+                        	<div class="row">
+                        		<div class="col-lg-8">
 						           <select class="form-control m-bot15">
 						                <option>Selecionar...</option>
 						                <option>DNI</option>
@@ -26,13 +24,7 @@
 						                <option>Direccion</option>
 						           </select>
                                 </div>
-                            
-                        </div>
-                    </div>
-                </form>
-            </div>
-							<div class="row">
-                                <div class="col-lg-6">
+                                <div class="col-lg-4">
                                 	<ul class="nav top-menu">                    
                                 		<li>
                         					<form class="navbar-form">
@@ -42,6 +34,12 @@
                 					</ul>  
                                 </div>
                             </div>
+                     	</div>
+                    </div>
+                </div>       
+                        
+            
+							
 			<div class="row">
                   <div class="col-lg-12">
                       <section class="panel">
@@ -52,16 +50,15 @@
                               <form class="form-horizontal " method="get">
                                                             
                                   <div class="form-group">
-                                      <label class="col-sm-2 control-label">Default</label>
+                                      <label class="col-sm-2 control-label">Apellido</label>
                                       <div class="col-sm-10">
                                           <input type="text" class="form-control">
                                       </div>
                                   </div>
                                   <div class="form-group">
-                                      <label class="col-sm-2 control-label">Help text</label>
+                                      <label class="col-sm-2 control-label">Nombre</label>
                                       <div class="col-sm-10">
                                           <input type="text" class="form-control">
-                                          <span class="help-block">A block of help text that breaks onto a new line and may extend beyond one line.</span>
                                       </div>
                                   </div>
                                   <div class="form-group">
@@ -103,31 +100,7 @@
                               </form>
                           </div>
                       </section>
-                      <section class="panel">
-                          <div class="panel-body">
-                              <form class="form-horizontal " method="get">
-                                  <div class="form-group has-success">
-                                      <label class="control-label col-lg-2" for="inputSuccess">Input with success</label>
-                                      <div class="col-lg-10">
-                                          <input type="text" class="form-control" id="inputSuccess">
-                                      </div>
-                                  </div>
-                                  <div class="form-group has-warning">
-                                      <label class="control-label col-lg-2" for="inputWarning">Input with warning</label>
-                                      <div class="col-lg-10">
-                                          <input type="text" class="form-control" id="inputWarning">
-                                      </div>
-                                  </div>
-                                  <div class="form-group has-error">
-                                      <label class="control-label col-lg-2" for="inputError">Input with error</label>
-                                      <div class="col-lg-10">
-                                          <input type="text" class="form-control" id="inputError">
-                                      </div>
-                                  </div>
-                              </form>
-                          </div>
-                      </section>
-                      
+                                            
                   </div>
             </div>
 		</div>
