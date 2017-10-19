54cb8ed605514bb8862f4a38c7a471d03905e63c
Gaston Schneider <gassstonn@gmail.com>
2017-10-19 19:02:07 -0300

    modificaciones

---
+++ gestor_persona_cargar_persona.php
@@ -0,0 +1,57 @@
+<?php //Simepre primero esto en todas las paginas exepto index
+      include_once "Plantilla.php";
+
+?>
+	
+	
+	<section id="main-content">
+        <section class="wrapper">            
+	    	<div class="row">
+	    		<div class="col-lg-12">
+					<h3 class="page-header"><i class="fa fa-file-text-o"></i>PERSONA</h3>
+				</div>
+            	<div class="row">
+                  <div class="col-lg-12" style="    padding-left: 15px;padding-right: 35px;">
+                      <section class="panel">
+                          <header class="panel-heading">
+                             DATOS
+                          </header>
+                          <div class="panel-body">
+                              <form class="form-horizontal " method="get">
+                                                            
+                                  <div class="form-group">
+                                      <label class="col-sm-2 control-label">APELLIDO</label>
+                                      <div class="col-sm-10">
+                                          <input type="text"  class="form-control" readonly="readonly">
+                                      </div>
+                                  </div>
+                                  <div class="form-group">
+                                      <label class="col-sm-2 control-label">NOMBRE</label>
+                                      <div class="col-sm-10">
+                                          <input type="text"  class="form-control" readonly="readonly">
+                                      </div>
+                                  </div>
+                                  <div class="form-group">
+                                      <label class="col-sm-2 control-label">EVENTO</label>
+                                      <div class="col-sm-10">
+                                          <input type="text"  class="form-control" readonly="readonly" style="width: 200px;">
+                                     </div>     
+                                  </div>
+                                  <div class="form-group">
+                                      <label class="col-sm-2 control-label">MESA</label>
+                                      <div class="col-sm-10">
+                                          <input type="text"  class="form-control" readonly="readonly" style="width: 200px;">
+                                      </div>
+                                  </div>
+                                  <div class="form-group">
+                                      <label class="col-sm-2 control-label">INVITADOS</label>
+                                      <div class="col-sm-10">
+                                          <input type="text"  class="form-control" readonly="readonly">
+                                      </div>
+                                  </div>                               
+                              </form>
+                          </div>
+                      </section>
+                                            
+                  </div>
+            </div>
